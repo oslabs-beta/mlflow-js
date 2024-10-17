003b2525472096eedc46f81d3a651fc3d9e4ab90
Kyler-Chiago <kchiago50@gmail.com>
2024-10-17 02:31:26 -0700

    basic website

--- mlflow-site/src/app/components/NavBar.tsx
+++ mlflow-site/src/app/components/NavBar.tsx
@@ -1,7 +1,17 @@
 const NavBar = () => {
   return (
-    <div className='navBar'>NavBar</div>
-  )
-}
+    <div className='navBar'>
+      <div>MLflow.js</div>
+      <div className='navBarLinks'>
+        <div>Home</div>
+        <div className='navBarLinksFeatures'>Features</div>
+        <div className='navBarLinksDemo'>Demo</div>
+        <div className='navBarLinksTeam'>Team</div>
+        <div className='navBarLinksGithub'>G</div>
+        <div className='navBarLinksLinkedIn'>L</div>
+      </div>
+    </div>
+  );
+};
 
-export default NavBar
+export default NavBar;
