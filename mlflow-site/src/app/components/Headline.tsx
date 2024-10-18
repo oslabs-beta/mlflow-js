d3c4995e3823dfd36cffe3f36a1e1b6ada4a4ce3
Kyler-Chiago <kchiago50@gmail.com>
2024-10-17 19:37:06 -0700

    before changing assets location

--- mlflow-site/src/app/components/Headline.tsx
+++ mlflow-site/src/app/components/Headline.tsx
@@ -3,7 +3,7 @@ import Button from './Button';
 const Headline = () => {
   return (
     <div className='home'>
-      <div>MLOps workflow for Javascript</div>
+      <div className='bigHeadline'>MLOps workflow for Javascript</div>
       <div>Harness MLflow&apos;s MLOps functionality for your Javascript application with MLflow.js</div>
       <Button />
     </div>
--- mlflow-site/src/app/components/NavBar.tsx
+++ mlflow-site/src/app/components/NavBar.tsx
@@ -1,14 +1,29 @@
+import Image from 'next/image'
+
 const NavBar = () => {
   return (
     <div className='navBar'>
-      <div>MLflow.js</div>
+      <div className='navBarMlflow'>MLflow.js</div>
       <div className='navBarLinks'>
         <div>Home</div>
         <div className='navBarLinksFeatures'>Features</div>
         <div className='navBarLinksDemo'>Demo</div>
         <div className='navBarLinksTeam'>Team</div>
-        <div className='navBarLinksGithub'>G</div>
-        <div className='navBarLinksLinkedIn'>L</div>
+        <a
+          href='https://github.com/oslabs-beta/mlflow-js'
+          className='navBarLinksGithub'
+        >
+          {/* <img src='../../assets/Github_logo.png' alt='G'></img> */}
+          <Image
+          src='/assets/Github_logo.png'
+          width={50}
+          height={50}
+          alt='M'
+          />
+        </a>
+        <a href='' className='navBarLinksLinkedIn'>
+          L
+        </a>
       </div>
     </div>
   );
