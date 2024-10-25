e7688e3821032c37c94ba1b260a25934175897f8
Kyler-Chiago <kchiago50@gmail.com>
2024-10-24 19:59:10 -0700

    put ignores back in eslint

--- mlflow/eslint.config.mjs
+++ mlflow/eslint.config.mjs
@@ -7,6 +7,9 @@ export default tseslint.config(
   eslint.configs.recommended,
   ...tseslint.configs.recommended,
   {
+    ignores: ['docs', 'lib/**'],
+  },
+  {
     rules: {
       '@typescript-eslint/no-explicit-any': 'off',
       'no-console': 'off',
