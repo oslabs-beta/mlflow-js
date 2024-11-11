20b8ed306799877f823c6b157417225af9274b11
Austin Fraser <austinbfraser@gmail.com>
2024-11-10 22:49:15 -0800

    more progress on ExperimentClient.test.ts

--- mlflow/tests/ExperimentClient.test.ts
+++ mlflow/tests/ExperimentClient.test.ts
@@ -1,10 +1,22 @@
-import { describe, test, expect, beforeAll } from '@jest/globals';
+import { describe, test, expect, beforeAll, afterAll } from '@jest/globals';
 import ExperimentClient from '../src/tracking/ExperimentClient';
 import { ApiError } from '../src/utils/apiError';
+import { before } from 'node:test';
 
 describe('ExperimentClient', () => {
   let experimentClient: ExperimentClient;
-  // let experimentId: string;
+  let experimentId: string;
+  let experimentName: string;
+  const testIds: string[] = [];
+
+  type experiment = {
+    experiment_id: string;
+    name: string;
+    artifact_location: string;
+    lifecycle_stage: string;
+    last_update_time: string;
+    creation_time: string;
+  };
 
   beforeAll(async () => {
     // Add a small delay to ensure MLflow is fully ready
@@ -12,8 +24,10 @@ describe('ExperimentClient', () => {
     experimentClient = new ExperimentClient('http://127.0.0.1:5001');
 
     // Generate the experiment ID to be used generically in later tests
-    // const timestamp = Date.now();
-    // experimentId = await experimentClient.createExperiment(`Testing ${timestamp}`);
+    const timestamp = Date.now();
+    experimentName = `Testing ${timestamp}`;
+    experimentId = await experimentClient.createExperiment(experimentName);
+    testIds.push(experimentId);
   });
 
   describe('createExperiment', () => {
@@ -22,15 +36,12 @@ describe('ExperimentClient', () => {
       const testExperimentId = await experimentClient.createExperiment(
         `Test experiment ${timestamp}`
       );
+      testIds.push(testExperimentId);
       expect(typeof testExperimentId).toBe('string');
       expect(testExperimentId).toBeTruthy();
     });
 
-    xtest('should create an experiment, storing artifacts in the passed-in location', async () => {});
-
-    xtest('should create an experiment with passed-in tags', async () => {});
-
-    test('- Should throw errors if name is missing', async () => {
+    test('should throw error if name is missing', async () => {
       // @ts-expect-error: testing for missing arguments
       await expect(experimentClient.createExperiment()).rejects.toThrow(
         ApiError
@@ -41,67 +52,36 @@ describe('ExperimentClient', () => {
       );
     });
 
-    test('- Should handle API errors', async () => {
-      const invalid_id = 'invalid_id';
-
-      // the thrown error is specifically an instance of 'ApiError'
+    test('should throw error if name is already in use', async () => {
       await expect(
-        experimentClient.createExperiment(invalid_id)
+        experimentClient.createExperiment(experimentName)
       ).rejects.toThrow(ApiError);
-
-      // the thrown error message with invalid_id somewhere in the message
-      await expect(
-        experimentClient.createExperiment(invalid_id)
-      ).rejects.toThrow(
-        /Error creating experiment from tracking server:.+invalid_id/
-      );
     });
   });
 
   describe('searchExperiment', () => {
     beforeAll(async () => {
-      const num1 = Math.random().toString().slice(2, 11);
-      const name1 = `Search test ${num1}`;
-      await experimentClient.createExperiment(name1);
-
-      const num2 = Math.random().toString().slice(2, 11);
-      const name2 = `Search test ${num2}`;
-      await experimentClient.createExperiment(name2);
-
-      const num3 = Math.random().toString().slice(2, 11);
-      const name3 = `Search test ${num3}`;
-      await experimentClient.createExperiment(name3);
-
-      const num4 = Math.random().toString().slice(2, 11);
-      const name4 = `Search test ${num4}`;
-      await experimentClient.createExperiment(name4);
-
-      const num5 = Math.random().toString().slice(2, 11);
-      const name5 = `Search test ${num5}`;
-      await experimentClient.createExperiment(name5);
+      for (let i = 0; i < 5; i++) {
+        const num = Math.random().toString().slice(2, 11);
+        const name = `Search test ${num}`;
+        const search = await experimentClient.createExperiment(name);
+        testIds.push(search);
+      }
     });
 
-    test('should return', async () => {
-      type searchResults = {
-        experiment_id: string
-        name: string
-        artifact_location: string
-        lifecycle_stage: string
-        last_update_time: string
-        creation_time: string
-      };
+    test('should return valid search results', async () => {
       const results: {
-        experiments?: searchResults[]
-        next_page_token?: string
+        experiments?: experiment[];
+        next_page_token?: string;
       } = await experimentClient.searchExperiment(
         "name LIKE 'Search test%'",
         4
       );
-      
+
       expect(results.experiments).toBeDefined();
       expect(results.next_page_token).toBeDefined();
       expect(results.experiments).toHaveLength(4);
-      results.experiments?.forEach(result => {
+      results.experiments?.forEach((result) => {
         expect(result).toHaveProperty('experiment_id');
         expect(result).toHaveProperty('name');
         expect(result).toHaveProperty('artifact_location');
@@ -109,37 +89,107 @@ describe('ExperimentClient', () => {
         expect(result).toHaveProperty('last_update_time');
         expect(result).toHaveProperty('creation_time');
       });
-      const nextPageTokenIsString: boolean = typeof results.next_page_token === 'string'
-      expect(nextPageTokenIsString).toBeTruthy;
+      expect(typeof results.next_page_token).toBe('string');
+    });
+  });
 
-      // could write add'l tests to expect name1, name2, name3, etc
+  describe('getExperiment', () => {
+    test('should return experiment information', async () => {
+      const experiment = await experimentClient.getExperiment(experimentId);
+      expect(experiment).toHaveProperty('experiment_id');
+      expect(experiment).toHaveProperty('name');
+      expect(experiment).toHaveProperty('artifact_location');
+      expect(experiment).toHaveProperty('lifecycle_stage');
+      expect(experiment).toHaveProperty('last_update_time');
+      expect(experiment).toHaveProperty('creation_time');
     });
 
-    test('- Should throw errors if name is missing', async () => {
+    test('should throw error if experiment ID is missing', async () => {
       // @ts-expect-error: testing for missing arguments
-      await expect(experimentClient.createExperiment()).rejects.toThrow(
-        ApiError
-      );
+      await expect(experimentClient.getExperiment()).rejects.toThrow(ApiError);
+    });
+  });
+
+  describe('getExperimentByName', () => {
+    test('should return experiment information', async () => {
+      const experiment = await experimentClient.getExperimentByName(experimentName);
+      expect(experiment).toHaveProperty('experiment_id');
+      expect(experiment).toHaveProperty('name');
+      expect(experiment).toHaveProperty('artifact_location');
+      expect(experiment).toHaveProperty('lifecycle_stage');
+      expect(experiment).toHaveProperty('last_update_time');
+      expect(experiment).toHaveProperty('creation_time');
+    });
+
+    test('should throw error if experiment name is missing', async () => {
       // @ts-expect-error: testing for missing arguments
-      await expect(experimentClient.createExperiment()).rejects.toThrow(
-        /Error creating experiment from tracking server:/
-      );
+      await expect(experimentClient.getExperimentByName()).rejects.toThrow(ApiError);
     });
+  });
 
-    test('- Should handle API errors', async () => {
-      const invalid_id = 'invalid_id';
-
-      // the thrown error is specifically an instance of 'ApiError'
-      await expect(
-        experimentClient.createExperiment(invalid_id)
-      ).rejects.toThrow(ApiError);
-
-      // the thrown error message with invalid_id somewhere in the message
-      await expect(
-        experimentClient.createExperiment(invalid_id)
-      ).rejects.toThrow(
-        /Error creating experiment from tracking server:.+invalid_id/
+  describe('deleteExperiment', () => {
+    test('should delete an experiment', async () => {
+      const num = Math.random().toString().slice(2, 11);
+      const name = `Test experiment ${num}`;
+      const idToDelete = await experimentClient.createExperiment(name);
+      await experimentClient.deleteExperiment(idToDelete);
+      const results: {
+        experiments?: experiment[];
+        next_page_token?: string;
+      } = await experimentClient.searchExperiment(
+        `name LIKE '${idToDelete}'`,
+        4
       );
+      expect(results).toEqual({});
     });
+  });
+
+  describe('restoreExperiment', () => {
+    test('should restore a deleted experiment', async () => {
+      const num = Math.random().toString().slice(2, 11);
+      const name = `Test experiment ${num}`;
+      const idToDelete = await experimentClient.createExperiment(name);
+      await experimentClient.deleteExperiment(idToDelete);
+      await experimentClient.restoreExperiment(idToDelete);
+      const results: {
+        experiments?: experiment[];
+        next_page_token?: string;
+      } = await experimentClient.searchExperiment(
+        `name LIKE '${name}'`,
+        4
+      );
+      expect(results.experiments).toBeDefined();
+      expect(results.experiments).toHaveLength(1);
+    });
+  });
+
+  describe('updateExperiment', () => {
+    test('should update an experiment\'s name', async () => {
+      const num = Math.random().toString().slice(2, 11);
+      const name = `Test experiment ${num}`;
+      const exp = await experimentClient.createExperiment(name);
+      testIds.push(exp);
+      const updatedName = `${name}_UPDATE`
+      await experimentClient.updateExperiment(exp, updatedName);
+      const results: {
+        experiments?: experiment[];
+        next_page_token?: string;
+      } = await experimentClient.searchExperiment(
+        `name LIKE '${updatedName}'`,
+        4
+      );
+      expect(results.experiments).toBeDefined();
+      expect(results.experiments).toHaveLength(1);
+      expect(results.experiments?.[0].experiment_id).toBe(exp);
+    });
+  });
+
+  afterAll(async () => {
+    while (testIds.length > 0) {
+      const id = testIds.pop();
+      if (id) {
+        await experimentClient.deleteExperiment(id);
+      }
+    }
   });
 });
