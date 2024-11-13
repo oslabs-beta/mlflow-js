5f8112623671c3711520817fad05ba8f0ebf8311
Stephany Ho <shohk39@gmail.com>
2024-11-12 22:24:06 -0500

    Minor fixes on single quotes

--- mlflow-site/src/app/components/Team.tsx
+++ mlflow-site/src/app/components/Team.tsx
@@ -1,36 +1,36 @@
-import TeamCard from "./TeamCard";
+import TeamCard from './TeamCard';
 
 const Team = () => {
   const teamArray = [
     {
-      name: "Yiqun Zheng",
-      github: "https://github.com/yiqunzheng",
-      linkedIn: "https://www.linkedin.com/in/yiqunzheng/",
-      pfp: "",
+      name: 'Yiqun Zheng',
+      github: 'https://github.com/yiqunzheng',
+      linkedIn: 'https://www.linkedin.com/in/yiqunzheng/',
+      pfp: '',
     },
     {
-      name: "Kyler Chiago",
-      github: "https://github.com/Kyler-Chiago",
-      linkedIn: "https://www.linkedin.com/in/kyler-chiago/",
-      pfp: "/assets/kylerpfp.png",
+      name: 'Kyler Chiago',
+      github: 'https://github.com/Kyler-Chiago',
+      linkedIn: 'https://www.linkedin.com/in/kyler-chiago/',
+      pfp: '/assets/kylerpfp.png',
     },
     {
-      name: "Austin Fraser",
-      github: "https://github.com/austinbfraser",
-      linkedIn: "http://www.linkedin.com/in/austin-fraser",
-      pfp: "",
+      name: 'Austin Fraser',
+      github: 'https://github.com/austinbfraser',
+      linkedIn: 'http://www.linkedin.com/in/austin-fraser',
+      pfp: '',
     },
     {
-      name: "Stephany Ho",
-      github: "https://github.com/seneyu/",
-      linkedIn: "https://www.linkedin.com/in/stephanyho/",
-      pfp: "/assets/stephanypfp.png",
+      name: 'Stephany Ho',
+      github: 'https://github.com/seneyu/',
+      linkedIn: 'https://www.linkedin.com/in/stephanyho/',
+      pfp: '/assets/stephanypfp.png',
     },
     {
-      name: "Winston Ludlam",
-      github: "https://github.com/winjolu/",
-      linkedIn: "https://www.linkedin.com/in/wjludlam/",
-      pfp: "",
+      name: 'Winston Ludlam',
+      github: 'https://github.com/winjolu/',
+      linkedIn: 'https://www.linkedin.com/in/wjludlam/',
+      pfp: '',
     },
   ];
 
