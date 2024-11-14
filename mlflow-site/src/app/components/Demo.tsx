a10a523caf59e739cbaac667c4faebe45dc57e99
Austin Fraser <austinbfraser@gmail.com>
2024-11-14 15:39:42 -0800

    styling fixes, added 3rd demo, added winston prof pic

--- mlflow-site/src/app/components/Demo.tsx
+++ mlflow-site/src/app/components/Demo.tsx
@@ -5,10 +5,12 @@ const Demo = () => {
   const demoCardHeaders = [
     'Manage experiments',
     'Complete workflow',
+    'Visualize results in the MLflow UI'
   ];
   const demoCardBlurbs = [
     'Create experiments with MLflow.js.  Using built-in workflows, manage complex operations easily.',
     'Use MLflow.js to support a full ML project with TensorFlow.js.  Log hyperparameters and key metrics during each training step.  Evaluate model performance and register succesful models.',
+    'Once the run completes, the MLflow UI provides powerful visualization tools to analyze experiments.  Compare training and testing metrics across different runs to track performance patterns, or create custom charts that combine any logged hyperparameters and metrics to identify optimal model configurations'
   ];
   const demoCardVideos = [
     'https://player.vimeo.com/video/1023585657',
--- mlflow-site/src/app/components/DemoCard.tsx
+++ mlflow-site/src/app/components/DemoCard.tsx
@@ -1,3 +1,5 @@
+import Image from 'next/image';
+
 const DemoCard = ({
   header,
   blurb,
@@ -8,29 +10,48 @@ const DemoCard = ({
   header: string;
   video: string;
 }) => {
-  return (
-    <div className="demoCard">
-      <div style={{ padding: '56.25% 0 0 0', position: 'relative' }}>
-        <iframe
-          src={video}
-          style={{
-            position: 'absolute',
-            top: 0,
-            left: 0,
-            width: '100%',
-            height: '100%',
-          }}
-          allow="autoplay; fullscreen; picture-in-picture"
-          allowFullScreen
-          title="Vimeo Video Player"
-        />
+  if (video !== undefined) {
+    return (
+      <div className="demoCard">
+        <div style={{ padding: '56.25% 0 0 0', position: 'relative'}}>
+          <iframe
+            className="vimeoPlayer"
+            src={video}
+            style={{
+              position: 'absolute',
+              top: 0,
+              left: 0,
+              width: '100%',
+              height: '100%',
+            }}
+            allow="autoplay; fullscreen; picture-in-picture"
+            allowFullScreen
+            title="Vimeo Video Player"
+          />
+        </div>
+        <div className="demoCardText">
+          <div className="demoCardHeader">{header}</div>
+          <div className="demoCardInfo">{blurb}</div>
+        </div>
       </div>
-      <div className="demoCardText">
-        <div className="demoCardHeader">{header}</div>
-        <div className="demoCardInfo">{blurb}</div>
+    );
+  }
+  else {
+    return (
+      <div className="demoCard" id="MLflow-ui-screenshot">
+        <Image
+          src={'/assets/mlflow-ui-screenshot-new.png'}
+          width={1089}
+          height={845}
+          alt='MLflow UI'
+        ></Image>
+        <div className="demoCardText">
+          <div className="demoCardHeader">{header}</div>
+          <div className="demoCardInfo">{blurb}</div>
+        </div>
       </div>
-    </div>
-  );
+    );
+  };
 };
 
 export default DemoCard;
